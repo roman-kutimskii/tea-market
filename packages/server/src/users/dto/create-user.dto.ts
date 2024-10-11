0a820db9a8cc263b1badab3f2af63926accc6126
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-10-11 08:34:05 +0300

    Добавить Dto и Entitry для пользователя

--- packages/server/src/users/dto/create-user.dto.ts
+++ packages/server/src/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({ description: 'The name of the user', example: 'John Doe' })
+  name: string;
+
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'john.doe@example.com',
+  })
+  email: string;
+
+  @ApiProperty({
+    description: 'The phone number of the user',
+    example: '+1234567890',
+  })
+  phoneNumber: string;
+}
