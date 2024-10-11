aa4b3e84c7dfe5043bf978202405445d5f767ae4
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-10-11 13:26:17 +0300

    Добавить тесты для контроллера user

--- packages/server/src/users/users.controller.spec.ts
+++ packages/server/src/users/users.controller.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,15 +14,79 @@ describe('UsersController', () => {
       providers: [
         {
           provide: UsersService,
-          useValue: {},
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
         },
       ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a user', async () => {
+      const createUserDto: CreateUserDto = {
+        name: 'John',
+        email: 'john@example.com',
+        phoneNumber: '123456789',
+      };
+
+      await controller.create(createUserDto);
+
+      expect(service.create).toHaveBeenCalledWith(createUserDto);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should call service.findAll', async () => {
+      await controller.findAll();
+
+      expect(service.findAll).toHaveBeenCalled();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should call service.findOne with correct id', async () => {
+      const userId = '1';
+
+      await controller.findOne(userId);
+
+      expect(service.findOne).toHaveBeenLastCalledWith(+userId);
+    });
+  });
+
+  describe('update', () => {
+    it('should call service.update with correct parameters', async () => {
+      const userId = '1';
+      const updateUserDto: UpdateUserDto = {
+        name: 'new name',
+        email: 'new email',
+        phoneNumber: 'new phoneNumber',
+      };
+
+      await controller.update(userId, updateUserDto);
+
+      expect(service.update).toHaveBeenCalledWith(+userId, updateUserDto);
+    });
+  });
+
+  describe('remove', () => {
+    it('should call service.remove with correct id', async () => {
+      const userId = '1';
+
+      await controller.remove(userId);
+
+      expect(service.remove).toHaveBeenCalledWith(+userId);
+    });
   });
 });
