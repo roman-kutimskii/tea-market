2d0f816d36a688e534092c061637eb0700687cde
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-10-11 08:23:04 +0300

    Подключить базу данных

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,11 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import databaseConfig from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [databaseConfig] }), UsersModule],
+  imports: [
+    ConfigModule.forRoot({ load: [databaseConfig] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        synchronize: true, // ⚠️ WARNING: Set to false in production to avoid data loss
+        autoLoadEntities: true,
+      }),
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
