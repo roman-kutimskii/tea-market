a2965195a4ff1759fbe2c8a0e538a8d4a41f1a37
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-10-14 15:58:42 +0300

    Установить дополнительные библиотеки

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -6,4 +6,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
 });
