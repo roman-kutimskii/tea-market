28c4bc6969acf54a3dd0218d8b15c6bc3f078c76
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-01-15 14:23:12 +0300

    Рефакторинг ItemsService для обработки создания, получения и обновления элементов с обработкой ошибок

--- packages/server/src/items/items.service.ts
+++ packages/server/src/items/items.service.ts
@@ -1,37 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Item } from './entities/item.entity';
-import { Repository } from 'typeorm';
-import { SaleToItem } from 'src/sale-to-items/entities/sale-to-item.entity';
+import { EntityNotFoundError, Repository } from 'typeorm';
 
 @Injectable()
 export class ItemsService {
   constructor(
     @InjectRepository(Item)
     private itemsRepository: Repository<Item>,
-    @InjectRepository(SaleToItem)
-    private saleToItemsRepository: Repository<SaleToItem>,
   ) {}
 
   create(createItemDto: CreateItemDto) {
-    return 'This action adds a new item';
+    const item = this.itemsRepository.create(createItemDto);
+    return this.itemsRepository.save(item);
   }
 
   findAll() {
-    return `This action returns all items`;
+    return this.itemsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
+  async findOne(id: number) {
+    try {
+      const item = await this.itemsRepository.findOneByOrFail({ id });
+      return item;
+    } catch (error) {
+      if (error instanceof EntityNotFoundError) {
+        throw new NotFoundException(error);
+      }
+      throw error;
+    }
   }
 
-  update(id: number, updateItemDto: UpdateItemDto) {
-    return `This action updates a #${id} item`;
+  async update(id: number, updateItemDto: UpdateItemDto) {
+    try {
+      const item = await this.itemsRepository.findOneByOrFail({ id });
+      Object.assign(item, updateItemDto);
+      return this.itemsRepository.save(item);
+    } catch (error) {
+      if (error instanceof EntityNotFoundError) {
+        throw new NotFoundException(error);
+      }
+      throw error;
+    }
   }
 
   remove(id: number) {
-    return `This action removes a #${id} item`;
+    return this.itemsRepository.delete(id);
   }
 }
