9e8fa31199434d26e93fc5f0e698d5a80d7bce81
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-01-15 14:22:08 +0300

    Добавить импорт сущности SaleToItem в модуль Sales

--- packages/server/src/sales/sales.module.ts
+++ packages/server/src/sales/sales.module.ts
@@ -3,9 +3,13 @@ import { SalesService } from './sales.service';
 import { SalesController } from './sales.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Sale } from './entities/sale.entity';
+import { SaleToItem } from 'src/sale-to-items/entities/sale-to-item.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Sale])],
+  imports: [
+    TypeOrmModule.forFeature([Sale]),
+    TypeOrmModule.forFeature([SaleToItem]),
+  ],
   controllers: [SalesController],
   providers: [SalesService],
 })
