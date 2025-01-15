4661265e3b1c30b899e31a225c169f6158436b0a
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-01-15 12:14:54 +0300

    Добавить сущности и DTO для управления товарами, продажами и продавцами

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
+import { SellersModule } from './sellers/sellers.module';
+import { ItemsModule } from './items/items.module';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, SellersModule, ItemsModule, SalesModule],
   controllers: [AppController],
   providers: [AppService],
 })
