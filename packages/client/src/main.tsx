364e7fce83d9fcb35d64c7d8e3b7c929c28d5bfc
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-01-17 19:44:12 +0300

    Добавить CssBaseline для улучшения стилизации приложения

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -3,9 +3,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App.tsx";
+import CssBaseline from "@mui/material/CssBaseline";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>,
 );
