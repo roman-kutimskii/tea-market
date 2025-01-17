c3601571acf7bc7d4259658649d81110da7eadd1
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-01-17 20:00:42 +0300

    Добавить поддержку темы Material-UI с использованием ThemeProvider

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,10 +4,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App.tsx";
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./theme.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
---
+++ packages/client/src/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#5d4037",
+    },
+    secondary: {
+      main: "#ffb300",
+    },
+  },
+});
