d83cd68ff6215c8fd162dd7c0b7a42aa0a26d192
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-01-18 12:29:39 +0300

    Добавить поля email, passwordHash, role и временные метки в сущность пользователя

--- packages/server/src/users/entities/user.entity.ts
+++ packages/server/src/users/entities/user.entity.ts
@@ -1,10 +1,32 @@
 import { Sale } from 'src/sales/entities/sale.entity';
-import { Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('users')
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ name: 'password_hash' })
+  passwordHash: string;
+
+  @Column()
+  role: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 
   @OneToMany(() => Sale, (sale) => sale.seller)
   sales: Sale[];
