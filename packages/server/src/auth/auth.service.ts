cd273c57e000a372403240f645b25a062814f1ef
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-01-18 12:30:03 +0300

    Добавить функциональность аутентификации с контроллером и сервисом, включая DTO для входа пользователя

--- packages/server/src/auth/auth.service.ts
+++ packages/server/src/auth/auth.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { compare } from 'bcrypt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signIn(email: string, password: string): Promise<unknown> {
+    const user = await this.usersService.findOneByEmail(email);
+    if (!(await compare(password, user.passwordHash))) {
+      throw new UnauthorizedException();
+    }
+    const { passwordHash, ...result } = user;
+    return result;
+  }
+}
