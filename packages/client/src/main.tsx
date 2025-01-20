95f52fac653688fefdad334b7e5a6be7bdd8ff95
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-01-20 09:49:27 +0300

    Добавить поддержку маршрутизации с использованием react-router

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -11,12 +11,15 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </StrictMode>,
 );
