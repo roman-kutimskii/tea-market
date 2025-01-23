558bab7ae9e493f51b3ba22cb263a4570c18d1db
Roman Kutimskii <roman.kutimskii@gmail.com>
2025-01-23 21:08:19 +0300

    Добавить поддержку загрузки аватаров пользователей в формате base64 с использованием S3

--- packages/server/src/s3/s3.service.ts
+++ packages/server/src/s3/s3.service.ts
@@ -7,31 +7,37 @@ import { v4 as uuidv4 } from 'uuid';
 export class S3Service {
   private s3Client: S3Client;
   private bucketName: string;
+  private endpoint: string;
 
   constructor(private configService: ConfigService) {
+    this.bucketName = this.configService.getOrThrow<string>('s3.bucketName');
+    this.endpoint = this.configService.getOrThrow<string>('s3.endpoint');
     this.s3Client = new S3Client({
       credentials: {
         accessKeyId: this.configService.getOrThrow<string>('s3.accessKey'),
         secretAccessKey:
           this.configService.getOrThrow<string>('s3.secretAccessKey'),
       },
-      endpoint: this.configService.getOrThrow<string>('s3.endpoint'),
+      endpoint: this.endpoint,
+      region: 'ru-central1',
     });
-    this.bucketName = this.configService.getOrThrow<string>('s3.bucketName');
   }
 
-  uploadBase64Image(base64Image: string) {
+  async uploadBase64Image(base64Image: string) {
     const base64Data = base64Image.replace(/^data:image\/\w+;base64,/, '');
     const buffer = Buffer.from(base64Data, 'base64');
 
     const contentType = base64Image.match(/^data:(image\/\w+);base64,/)![1];
+    const fileName = `${Date.now().toString()}-${uuidv4()}.jpg`;
     const command = new PutObjectCommand({
       Bucket: this.bucketName,
-      Key: `${Date.now().toString()}-${uuidv4}.jpg`,
+      Key: fileName,
       Body: buffer,
       ContentType: contentType,
     });
 
-    return this.s3Client.send(command);
+    await this.s3Client.send(command);
+
+    return `${this.endpoint}/${this.bucketName}/${fileName}`;
   }
 }
